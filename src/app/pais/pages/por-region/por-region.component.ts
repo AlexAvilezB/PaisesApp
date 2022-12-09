a942b88f62b5b7f46704b489a55bf9a38baa931a
Alejandro Avilez <aleantonio656@gmail.com>
2022-12-09 17:09:00 -0600

    Funcionalidad de busqueda por regiones

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,14 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais-interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
-  templateUrl: './por-region.component.html'
+  templateUrl: './por-region.component.html',
+  styles: [
+    `
+      button {
+        margin: 5px 5px 0 0;
+      }
+    `,
+  ],
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+  regiones: string[] = [
+    'EU',
+    'EFTA',
+    'CARICOM',
+    'PA',
+    'AU',
+    'USAN',
+    'EEU',
+    'AL',
+    'ASEAN',
+    'CAIS',
+    'CEFTA',
+    'NAFTA',
+    'SAARC'
+  ];
+  regionActiva: string = '';
+  paises: Country[] = [];
 
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
-  ngOnInit(): void {
+  getClaseCSS(region: string) {
+    return region === this.regionActiva
+      ? 'btn btn-primary'
+      : 'btn btn-outline-primary';
   }
 
+  activarRegion(region: string) {
+    this.regionActiva = region;
+    this.paisService.buscarRegion( this.regionActiva ).subscribe( paises => {
+      this.paises = paises;
+    })
+  }
 }
